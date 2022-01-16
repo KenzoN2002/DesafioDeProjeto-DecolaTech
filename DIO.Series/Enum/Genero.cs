064bbe230d6c56f4ef9726ea21a4c8b65d57c692
KenzoN2002 <owamlima92@gmail.com>
2022-01-16 00:10:16 -0300

    implementado apenas o demonstrado, faltando pontos de melhoria

---
+++ DIO.Series/Enum/Genero.cs
@@ -0,0 +1,18 @@
+namespace DIO.Series{
+    public enum Genero{
+        Acao = 1 ,
+        Aventura = 2,
+        Comedia = 3,
+        Documentario = 4,
+        Drama = 5,
+        Espionagem = 6,
+        Faroeste = 7,
+        fantasi = 8,
+        Ficcao_cinetifica = 9,
+        musical = 10,
+        romance = 11,
+        Suspense = 12,
+        Terror = 13,
+        Animacao = 14
+    }
+}
